08b6dfbe91f69c3b52bca50d82c94fb0d5dc33c8
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-07-28 16:53:39 +0200

    invasive customization using attributes

--- src/BsonSerialization.Sample/TopLevel.cs
+++ src/BsonSerialization.Sample/TopLevel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace BsonSerialization.Sample
 {
 	public class TopLevel
 	{
 		public Guid Id { get; set; }
+		[BsonElement("display_name")]
 		public string Name { get; set; }
 		public decimal Number { get; set; }
 		public SecondLevel[] Children { get; set; }
