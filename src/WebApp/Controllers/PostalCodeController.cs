1e9daf18936b3ebbfe0372183c8c06563a862b3f
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-09-02 14:59:04 +0200

    show 24 first postal codes using bsondocument

---
+++ src/WebApp/Controllers/PostalCodeController.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using Nancy;
+
+namespace UseDynamicSyntax.Controllers
+{
+	public class PostalCodeController : NancyModule
+	{
+		public PostalCodeController()
+		{
+			var db = new MongoClient().GetServer().GetDatabase("dynamic");
+
+			Get["/postal-codes"] = p =>
+			{
+				var first24 = db.GetCollection("PostalCodes").FindAll().SetLimit(24);
+
+				return View["PostalCode/Index", first24];
+			};
+		}
+	}
+}
