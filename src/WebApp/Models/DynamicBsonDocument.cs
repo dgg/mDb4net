902d329371b02fafe226ccac1cdcd63268f9741f
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2014-11-19 15:11:33 +0100

    fix code to support arrays of documents

--- src/WebApp/Models/DynamicBsonDocument.cs
+++ src/WebApp/Models/DynamicBsonDocument.cs
@@ -20,13 +20,29 @@ namespace UseDynamicSyntax.Models
 			BsonValue value;
 			if (_doc.TryGetValue(binder.Name, out value))
 			{
-				result = value.IsBsonDocument ?
-					(object)new DynamicBsonDocument(value.AsBsonDocument) :
-					value;
+				result = wrap(value);
 				return true;
 			}
 			result = null;
 			return true;
+		}
+
+		public object wrap(BsonValue value)
+		{
+			object wrapped;
+			if (value.IsBsonDocument)
+			{
+				wrapped = new DynamicBsonDocument(value.AsBsonDocument);
+			}
+			else if (value.IsBsonArray)
+			{
+				wrapped = value.AsBsonArray.Select(wrap).ToArray();
+			}
+			else
+			{
+				wrapped = value;
+			}
+			return wrapped;
 		}
 
 		public override IEnumerable<string> GetDynamicMemberNames()
